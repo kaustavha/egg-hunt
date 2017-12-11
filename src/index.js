de94e3cbff19d359be9a574db980b366ec907d54
Hernando Castano <castano.ha@gmail.com>
2017-12-10 22:19:23 -0500

    Allow a range of blocks to be searched for events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 var Web3 = require('web3')
 
 const web3 = new Web3('http://localhost:8545')
+// web3.setProvider(new Web3.providers.WebsocketProvider('ws://localhost:8546'));
+// new Web3.providers.WebsocketProvider("ws://localhost:8545")
 
 console.log("Running Web3 Version: ", web3.version)
 
@@ -19,19 +21,54 @@ console.log("Running Web3 Version: ", web3.version)
  *  Once events are up-to-date, subscibe for new events
  *  Keep storing new events in DB until AWS gets angry
  *
+ * Might want to turn the logic into some sort of state machine
+ * Would allow us to pick up from where we left off reading blocks
  */
 
 async function getBlockInfo(blockNum) {
-  var res = await web3.eth.getBlock(blockNum)
-  return res
+  var info = await web3.eth.getBlock(blockNum)
+  return info
 }
 
-getBlockInfo(14).then(res => {
-  var blockTxs = res.transactions
-  console.log(res)
-  console.log(blockTxs)
-})
+async function getTxReceipt(transaction) {
+  var receipt = await web3.eth.getTransactionReceipt(transaction)
+  return receipt
+}
 
-web3.eth.getBlockTransactionCount(1).then(count => {
-  console.log("Count: ", count)
-})
+async function getTransactionCount(blockNum) {
+  var count = await web3.eth.getBlockTransactionCount(blockNum)
+  return count
+}
+
+async function getEventFromLogs(receipt) {
+  var eventString
+  for (let i = 0; i < receipt.logs.length; i++) {
+    eventString = receipt.logs[i].data
+    console.log("Event String:", web3.utils.toAscii(eventString))
+  }
+}
+
+async function extractEvents(startBlock, endBlock) {
+  var blockInfo, transactionCount
+  var transactions, transactionReceipt
+  for (var i = startBlock; i <= endBlock; i++) {
+    blockInfo = await getBlockInfo(i)
+    transactions = blockInfo.transactions
+    console.log("Block #", i, "transaction list:", transactions)
+
+    // Sorry for nesting loops like this, there's no better way :(
+    for (let tx of transactions) {
+      console.log("Transaction:", tx)
+      if (tx != undefined) {
+        transactionReceipt = await getTxReceipt(tx)
+        console.log("TxReceipt: ", transactionReceipt)
+        getEventFromLogs(transactionReceipt)
+      }
+    }
+
+    // console.log(blockInfo)
+  }
+}
+
+// Block 39 Should have a meaningful Honk
+extractEvents(35, 39)
